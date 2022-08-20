c74abc799e234f96147f0386e64fff8bb264794d
Animesh Kahara <animeshcyberphysco@gmail.com>
2022-08-21 01:16:52 +0530

    Base Setup with Html, Css, JS.

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function () {
+  //canvas setup
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+
+  //setting up canvas height and width
+  canvas.height = 500;
+  canvas.width = 500;
+});
